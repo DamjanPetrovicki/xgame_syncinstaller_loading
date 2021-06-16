acaf21c4e1bce90753629356a43d56e8ed330f69
sync <50116007+synceye@users.noreply.github.com>
2021-06-16 22:20:49 +0200

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,6 @@
+fx_version 'cerulean'
+game 'gta5'
+
 files {
     'index.html',
     'music/loading.ogg',
@@ -35,5 +38,3 @@ files {
 loadscreen 'index.html'
 loadscreen_manual_shutdown 'yes'
 client_script 'client.lua'
-
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
